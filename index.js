9147e0dd6aefd8dbd8953dd64311958db6447514
alwysaid <alwysaid@icloud.com>
2024-04-11 20:12:23 +0700

    add alwy ver 2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 perkenalkan saya samna
-saya baru lala
+saya baru lalalala
 
 
 
@@ -7,9 +7,9 @@ saya baru lala
 
 git status
 git add = untuk menambahkan perubahan baru di working stage (git add . / git add "nama file")
-git commit -m
+git commit -m "judul"
 git push origin main
 git checkout -b "nama branch" = untuk bikin branch baru
 git branch -a = untuk cek branch
-
+git push origin "nama branch"
 */
